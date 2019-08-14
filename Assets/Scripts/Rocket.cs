fe0b472efeb71c9f27eba6c8a6fe6cdd2993b9ca
Yuri Aguiar <yuriaguiar011@gmail.com>
2019-08-14 20:42:53 -0300

    Created death and reload scene events, added sound to the thrust and created a second level

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,9 +1,18 @@
-using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
+    enum State
+    {
+        Alive,
+        Dying,
+        Transition
+    }
+
+    private State state = State.Alive;
     private Rigidbody _rigidbody;
+    private AudioSource _audioSource;
 
     /// <summary>
     /// Start is called on the frame when a script is enabled just before
@@ -12,6 +21,7 @@ public class Rocket : MonoBehaviour
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     /// <summary>
@@ -19,23 +29,80 @@ public class Rocket : MonoBehaviour
     /// </summary>
     void Update()
     {
-        ProcessInput();
+        if (state == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }
     }
 
-    private void ProcessInput()
+    [SerializeField] float _mainThrust = 100f;
+    private void Thrust()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.W))
         {
-            _rigidbody.AddRelativeForce(Vector3.up);
+            _rigidbody.AddRelativeForce(Vector3.up * _mainThrust * Time.deltaTime);
+
+            if (!_audioSource.isPlaying)
+            {
+                _audioSource.Play();
+            }
         }
+        else
+        {
+            _audioSource.Stop();
+        }
+    }
+
+    [SerializeField] float _thrustForce = 1f;
+    private void Rotate()
+    {
+        float rotationThisFrame = _thrustForce * Time.deltaTime;
+        _rigidbody.freezeRotation = true;
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * _thrustForce);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * _thrustForce);
         }
+
+        _rigidbody.freezeRotation = false;
+    }
+
+    /// <summary>
+    /// OnCollisionEnter is called when this collider/rigidbody has begun
+    /// touching another rigidbody/collider.
+    /// </summary>
+    /// <param name="other">The Collision data associated with this collision.</param>
+    void OnCollisionEnter(Collision other)
+    {
+        if (state != State.Alive) { return; }
+
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                break;
+            case "Finish":
+                state = State.Transition;
+                Invoke("LoadNextLvl", 1f);
+                break;
+            default:
+                state = State.Dying;
+                Invoke("LoadFirstLvl", 1f);
+                break;
+        }
+    }
+
+    private void LoadNextLvl()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void LoadFirstLvl()
+    {
+        SceneManager.LoadScene(0);
     }
 }
